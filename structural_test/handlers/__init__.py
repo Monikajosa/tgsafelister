56d88f282ac3afae754cc9a935348d9727bf8ab7
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2024-12-30 12:23:37 +0100

    Update __init__.py

--- structural_test/handlers/__init__.py
+++ structural_test/handlers/__init__.py
@@ -1,2 +1,13 @@
 # handlers/__init__.py
-# Diese Datei kann leer bleiben, signalisiert aber Python, dass dies ein Paket ist.
+from .start import start
+from .main_menu import handle_main_menu
+from .report import user_selected, receive_full_name, receive_username, receive_reason, handle_update_choice, cancel
+from .check import check_user
+from .error_handler import error_handler
+from .deletion import delete_user, receive_deletion_info
+from .support import handle_support_message
+
+# Importieren Sie die Zustände, falls sie in einer der Dateien definiert sind
+from .report import SELECTING_USER, WAITING_FOR_FULL_NAME, WAITING_FOR_USERNAME, WAITING_FOR_REASON, UPDATING_USER
+from .check import CHECKING_LIST
+from .deletion import WAITING_FOR_DELETION_INFO
