86cf57d301b1be7a7b2a4c5e3d33654d30012dc0
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2024-12-30 12:07:53 +0100

    Create utils.py

---
+++ structural_test/handlers/utils.py
@@ -0,0 +1,26 @@
+import json
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+
+def load_data():
+    try:
+        with open('reported_users.json', 'r') as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {"scammers": {}, "trusted": {}}
+
+def save_data():
+    with open('reported_users.json', 'w') as f:
+        json.dump(reported_users, f, indent=4)
+
+def get_main_keyboard():
+    keyboard = [
+        [KeyboardButton("Scammer melden"), KeyboardButton("Trust melden")],
+        [KeyboardButton("User prüfen")],
+        [KeyboardButton("Löschung beantragen")]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+def escape_markdown(text):
+    """Escapes markdown special characters."""
+    escape_chars = r"\_*[]()~`>#+-=|{}.!"
+    return ''.join(f'\\{char}' if char in escape_chars else char for char in text)
