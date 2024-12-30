a026d10c3468c5340c7315b5926ce7eda6e4f7b5
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2024-12-30 16:18:04 +0100

    Update bot.py

--- structural_test/bot.py
+++ structural_test/bot.py
@@ -1,8 +1,10 @@
 import logging
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
 from config import TOKEN, SUPPORT_GROUP_ID, OWNER_ID
-from handlers import start, handle_main_menu, user_selected, check_user, handle_update_choice, receive_full_name, receive_username, receive_reason, cancel, receive_deletion_info, handle_support_message, error_handler, delete_user
+from handlers import start, handle_main_menu, user_selected, check_user, handle_update_choice, receive_full_name, receive_username, receive_reason, cancel, handle_support_message, error_handler, delete_user
 from handlers import SELECTING_USER, WAITING_FOR_FULL_NAME, WAITING_FOR_USERNAME, WAITING_FOR_REASON, UPDATING_USER, WAITING_FOR_DELETION_INFO, CHECKING_LIST
+from handlers.deletion_request import request_deletion, receive_deletion_info  # Importiere die Funktionen
+from handlers.utils import get_main_keyboard  # Importiere die get_main_keyboard Funktion
 
 # Logging konfigurieren
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -38,7 +40,7 @@ def main() -> None:
 
     deletion_conv_handler = ConversationHandler(
         entry_points=[
-            MessageHandler(filters.Regex('^(Löschung beantragen)$'), handle_main_menu)
+            MessageHandler(filters.Regex('^(Löschung beantragen)$'), request_deletion)
         ],
         states={
             WAITING_FOR_DELETION_INFO: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_deletion_info)]
