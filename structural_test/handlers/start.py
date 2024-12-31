fe672bbd48cbde780162c9044586785f447c5eff
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2024-12-31 22:05:22 +0100

    Update start.py

--- structural_test/handlers/start.py
+++ structural_test/handlers/start.py
@@ -1,10 +1,12 @@
-from telegram import Update
+import logging
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
-from .utils import get_main_keyboard, load_data
+from handlers.utils import get_main_keyboard, load_data
 
-reported_users = load_data()
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    reported_users = load_data()
     total_reports = sum(user_data['count'] for list_type in reported_users for user_data in reported_users[list_type].values())
     await update.message.reply_text(
         f'👋 Willkommen beim Telegram Sicherheits-Bot! \n\n'
