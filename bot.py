d809b6e72dc239e91adf1fad9e93a5cfa7d0e332
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2025-01-05 09:55:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,14 @@
 import logging
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
 from config import TOKEN, SUPPORT_GROUP_ID, OWNER_ID
-from handlers import start, handle_main_menu, user_selected, check_user, handle_update_choice, receive_full_name, receive_username, receive_reason, cancel, handle_support_message, error_handler, delete_user
+from handlers import start, handle_main_menu, user_selected, check_user, handle_update_choice, receive_full_name, receive_username, receive_reason, cancel, receive_deletion_info, handle_support_message, error_handler, delete_user
 from handlers import SELECTING_USER, WAITING_FOR_FULL_NAME, WAITING_FOR_USERNAME, WAITING_FOR_REASON, UPDATING_USER, WAITING_FOR_DELETION_INFO, CHECKING_LIST
-from handlers.deletion_request import request_deletion, receive_deletion_info, deletion_conv_handler
-from handlers.utils import get_main_keyboard
 
 # Logging konfigurieren
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Timeout-Wert
-TIMEOUT = 60  # Timeout-Wert in Sekunden
-
 def main() -> None:
-    global support_message_mapping, deletion_requests
-    support_message_mapping = {}
-    deletion_requests = {}
-
     application = Application.builder().token(TOKEN).build()
 
     report_conv_handler = ConversationHandler(
@@ -41,8 +32,19 @@ def main() -> None:
         ],
         states={
             CHECKING_LIST: [MessageHandler(filters.StatusUpdate.USER_SHARED, check_user)]
-        ],
+        },
         fallbacks=[CommandHandler("cancel", cancel)]
+    )
+
+    deletion_conv_handler = ConversationHandler(
+        entry_points=[
+            MessageHandler(filters.Regex('^(Löschung beantragen)$'), handle_main_menu)
+        ],
+        states={
+            WAITING_FOR_DELETION_INFO: [MessageHandler(filters.TEXT & ~filters.COMMAND, receive_deletion_info)]
+        },
+        fallbacks=[CommandHandler("cancel", lambda update, context: update.message.reply_text("Meldung abgebrochen.",
+                                                                                              reply_markup=get_main_keyboard()))]
     )
 
     application.add_handler(CommandHandler("start", start))
